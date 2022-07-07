a98b037e1fed179c7f2743edacab0557998b7b09
Helen Scott <helengracescott@gmail.com>
2022-07-07 11:23:39 -0400

    New function for reading L4

---
+++ R/readLevel4A.R
@@ -0,0 +1,34 @@
+#'Read GEDI Level1B data (Geolocated Waveforms)
+#'
+#'@description This function reads GEDI level1B products: geolocated Waveforms
+#'
+#'@usage readLevel1B(level1Bpath)
+#'
+#'@param level1Bpath File path pointing to GEDI level1B data. Data in HDF5 Hierarchical Data Format (.h5).
+#'
+#'@return Returns an S4 object of class [`gedi.level1b-class`] containing GEDI level1B data.
+#'
+#'@seealso [`hdf5r::H5File-class`] in the \emph{hdf5r} package and
+#'\url{https://lpdaac.usgs.gov/products/gedi01_bv002/}
+#'
+#'@examples
+#'# Specifying the path to GEDI level1B data (zip file)
+#'outdir = tempdir()
+#'level1B_fp_zip <- system.file("extdata",
+#'                   "GEDI01_B_2019108080338_O01964_T05337_02_003_01_sub.zip",
+#'                   package="rGEDI")
+#'
+#'# Unzipping GEDI level1B data
+#'level1Bpath <- unzip(level1B_fp_zip,exdir = outdir)
+#'
+#'# Reading GEDI level1B data (h5 file)
+#'level1b<-readLevel1B(level1Bpath=level1Bpath)
+#'
+#'close(level1b)
+#'@import hdf5r
+#'@export
+readLevel4a <-function(level4apath) {
+  level4a_h5 <- hdf5r::H5File$new(level4apath, mode = 'r')
+  level4a<- new("gedi.level4a", h5 = level4a_h5)
+  return(level4a)
+}
